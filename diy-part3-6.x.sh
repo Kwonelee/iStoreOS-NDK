6248d23240414a0ac9753de8e9ccb5e489d012eb
Kwonelee <995546054@qq.com>
2025-08-29 12:26:00 +0800

    Update diy-part3-6.x.sh

--- diy-part3-6.x.sh
+++ diy-part3-6.x.sh
@@ -7,39 +7,6 @@
 # Blog: https://p3terx.com
 #===============================================
 
-# 修改uhttpd配置文件，启用nginx
-# sed -i "/.*uhttpd.*/d" .config
-# sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
-# sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
-#sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
-#sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
-#cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
-# ls package/base-files/files/etc/
-
-# 追加binder内核参数
-#echo "CONFIG_PSI=y
-#CONFIG_KPROBES=y" >> target/linux/rockchip/armv8/config-6.6
-
-# 集成CPU性能跑分脚本
-#cp -a $GITHUB_WORKSPACE/configfiles/coremark/* package/base-files/files/bin/
-#chmod 755 package/base-files/files/bin/coremark
-#chmod 755 package/base-files/files/bin/coremark.sh
-
-# 集成无线
-mkdir -p package/base-files/files/lib/firmware/brcm/
-cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib/firmware/brcm/
-
-# 增加m2设备
-echo -e "\\ndefine Device/firefly_station-m2
-\$(call Device/Legacy/rk3566,\$(1))
-  DEVICE_VENDOR := Firefly
-  DEVICE_MODEL := Station M2 / RK3566 ROC PC
-  DEVICE_DTS := rk3568/rk3566-roc-pc
-  SUPPORTED_DEVICES += rockchip,rk3566-roc-pc firefly,rk3566-roc-pc firefly,station-m2
-  DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
-endef
-TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/legacy.mk
-
 # 增加tv设备
 echo -e "\\ndefine Device/rk3399_tvi3315a
   $(Device/Legacy)
@@ -52,29 +19,29 @@ echo -e "\\ndefine Device/rk3399_tvi3315a
 endef
 TARGET_DEVICES += rk3399_tvi3315a" >> target/linux/rockchip/image/legacy.mk
 
-# 替换uboot-rockchip/Makefile
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
+echo -e "\\ndefine Device/rk3399_tvi3315a
+  $(Device/Legacy)
+  DEVICE_VENDOR := RK3399
+  DEVICE_MODEL := tvi3315a
+  SOC := rk3399
+  SUPPORTED_DEVICES := rk3399,tvi3315a
+  DEVICE_DTS := rockchip/rk3399-tvi3315a
+  IMAGE/idbloader.bin := cat bin/target/rockchip/rk3399-tvi3315a/idbloader.bin
+  IMAGE/trust.bin := cat bin/target/rockchip/rk3399-tvi3315a/trust.bin
+  IMAGE/uboot.img := cat bin/target/rockchip/rk3399-tvi3315a/uboot.img
+endef
+TARGET_DEVICES += rk3399_tvi3315a" >> target/linux/rockchip/image/legacy.mk
 
-# 复制defconfig配置文件到uboot-rockchip
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 
-# 复制dts设备树文件到指定目录下
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts target/linux/rockchip/dts/rk3568/
 
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399.dtsi target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-opp-cp.dtsi target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
-# 以及package/boot/uboot-rockchip/src/arch/arm/dts/目录
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-opp-cp.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 
-# 添加dtb补丁
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/900-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
-
-# 测试添加luci-app-turboacc
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
-
-# 定时限速插件
-#git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+# 增加m2设备
+echo -e "\\ndefine Device/firefly_station-m2
+\$(call Device/Legacy/rk3566,\$(1))
+  DEVICE_VENDOR := Firefly
+  DEVICE_MODEL := Station M2 / RK3566 ROC PC
+  DEVICE_DTS := rk3568/rk3566-roc-pc
+  SUPPORTED_DEVICES += rockchip,rk3566-roc-pc firefly,rk3566-roc-pc firefly,station-m2
+  DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
+endef
+TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/legacy.mk
