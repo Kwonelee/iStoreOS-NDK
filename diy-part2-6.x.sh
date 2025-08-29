b1b4901d7fd1592b28b30a835837ac6dece45a69
Kwonelee <995546054@qq.com>
2025-08-29 23:40:05 +0800

    Update diy-part2-6.x.sh

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -54,14 +54,14 @@ TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
 # 替换uboot-rockchip/Makefile
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
 
-# 复制defconfig配置文件到uboot-rockchip
+# 复制配置文件到uboot-rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 
 # 复制dts设备树文件到指定目录下
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts target/linux/rockchip/dts/rk3568/
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts target/linux/rockchip/dts/rk3399/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts package/boot/uboot-rockchip/src/arch/arm/dts/
 
 # 添加dtb补丁
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/900-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
