a1625a84cf6e15c69ae022d0747e2f9ad189412c
Kwonelee <995546054@qq.com>
2025-08-30 14:11:13 +0800

    Update diy-part3-6.x-bak.sh

--- diy-part3-6.x-bak.sh
+++ diy-part3-6.x-bak.sh
@@ -34,6 +34,35 @@ cp -f $GITHUB_WORKSPACE/configfiles/patch/900-add-rk3399-tvi3315a-dtb-to-makefil
 
 
 
+###RK3399移植调整文件如下（方式一），单网口不需要改网络配置文件
+
+# 增加tv设备
+echo -e "\\ndefine Device/tvi_tvi3315a
+  DEVICE_VENDOR := Tvi
+  DEVICE_MODEL := TVI3315A
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := tvi3315a-rk3399
+endef
+TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
+
+# 替换uboot-rockchip/Makefile
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
+
+# 复制配置文件到uboot-rockchip
+cp -a $GITHUB_WORKSPACE/configfiles/dts/rk3399/* package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+
+# 复制dts设备树文件到指定目录下
+cp -a $GITHUB_WORKSPACE/configfiles/dts/rk3399/* target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+
+# 添加dtb补丁
+cp -f $GITHUB_WORKSPACE/configfiles/patch/900-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
+
+
+
+
+
 ### 官方提供继承的两个默认配置参考 ###
 define Device/Default
   PROFILES := Default
