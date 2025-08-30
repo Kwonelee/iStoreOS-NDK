11319260cb85ce8e8fd6b0b3bae381afb36d6ce1
Kwonelee <995546054@qq.com>
2025-08-30 09:02:26 +0800

    更新 diy-part2-6.x.sh

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -35,7 +35,7 @@ echo -e "\\ndefine Device/firefly_station-m2
   DEVICE_VENDOR := Firefly
   DEVICE_MODEL := Station M2 / RK3566 ROC PC
   DEVICE_DTS := rk3568/rk3566-roc-pc
-  SUPPORTED_DEVICES += rockchip,rk3566-roc-pc firefly,rk3566-roc-pc firefly,station-m2
+  SUPPORTED_DEVICES += firefly,rk3566-roc-pc firefly,station-m2
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
 endef
 TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/legacy.mk
