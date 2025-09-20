22d53c1d80ff65ac663f32de9a84b09b24c35fa2
Kwonelee <995546054@qq.com>
2025-09-20 21:55:59 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -119,7 +119,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/l
 
 # UPnP
 rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
-git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7
+git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.9
 git clone https://git.kejizero.online/zhao/luci-app-upnp feeds/luci/applications/luci-app-upnp -b master
 
 # 必要的补丁
