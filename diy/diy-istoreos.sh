39fae2577fed623235474b192c2f9b0b7047ddda
Kwonelee <995546054@qq.com>
2025-09-20 22:06:11 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -1,4 +1,58 @@
 #!/bin/bash
+
+# 集成无线
+mkdir -p package/base-files/files/lib/firmware/brcm
+cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib/firmware/brcm/
+
+# ================================================================
+# 移植RK3399示例，其他RK3399可模仿
+# ================================================================
+# 增加tvi3315a设备
+echo -e "\\ndefine Device/tvi_tvi3315a
+  DEVICE_VENDOR := Tvi
+  DEVICE_MODEL := TVI3315A
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := tvi3315a-rk3399
+endef
+TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
+
+# 替换package/boot/uboot-rockchip/Makefile
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
+
+# 复制dts与配置文件到package/boot/uboot-rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+
+# 复制dts到files/arch/arm64/boot/dts/rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+
+# 添加dtb补丁到target/linux/rockchip/patches-6.6
+cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
+# ================================================================
+# RK3399示例结束
+# ================================================================
+
+# ================================================================
+# 移植RK3566示例，其他RK35xx可模仿
+# ================================================================
+# 增加jp-tvbox设备
+echo -e "\\ndefine Device/jp_jp-tvbox
+\$(call Device/Legacy/rk3566,\$(1))
+  DEVICE_VENDOR := Jp
+  DEVICE_MODEL := JP TVBOX
+  DEVICE_DTS := rk3568/rk3566-jp-tvbox
+  SUPPORTED_DEVICES += jp,jp-tvbox
+  DEVICE_PACKAGES := kmod-scsi-core
+endef
+TARGET_DEVICES += jp_jp-tvbox" >> target/linux/rockchip/image/legacy.mk
+
+# 复制dts到target/linux/rockchip/dts/rk3568
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/
+# ================================================================
+# RK35xx示例结束
+# ================================================================
+
 mkdir -p files/etc/config
 wget -qO- https://raw.githubusercontent.com/Kwonelee/Kwonelee/refs/heads/main/rule/openclash > files/etc/config/openclash
 #wget -qO- https://raw.githubusercontent.com/sos801107/TL-XDR608X/refs/heads/main/etc/mosdns > files/etc/config/mosdns
